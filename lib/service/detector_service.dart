aaca73c6e971e264e04ed3de50e1e1abff0dc427
Anand Doshi <anand.doshi20@imperial.ac.uk>
2025-09-26 23:27:34 +0100

    init commit

---
+++ lib/service/detector_service.dart
@@ -0,0 +1,462 @@
+// Copyright 2023 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+import 'dart:isolate';
+import 'dart:math';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as image_lib;
+import 'package:guitar_guide/models/recognition.dart';
+import 'package:guitar_guide/utils/image_utils.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+///////////////////////////////////////////////////////////////////////////////
+// **WARNING:** This is not production code and is only intended to be used for
+// demonstration purposes.
+//
+// The following Detector example works by spawning a background isolate and
+// communicating with it over Dart's SendPort API. It is presented below as a
+// demonstration of the feature "Background Isolate Channels" and shows using
+// plugins from a background isolate. The [Detector] operates on the root
+// isolate and the [_DetectorServer] operates on a background isolate.
+//
+// Here is an example of the protocol they use to communicate:
+//
+//  _________________                         ________________________
+//  [:Detector]                               [:_DetectorServer]
+//  -----------------                         ------------------------
+//         |                                              |
+//         |<---------------(init)------------------------|
+//         |----------------(init)----------------------->|
+//         |<---------------(ready)---------------------->|
+//         |                                              |
+//         |----------------(detect)--------------------->|
+//         |<---------------(busy)------------------------|
+//         |<---------------(result)----------------------|
+//         |                 . . .                        |
+//         |----------------(detect)--------------------->|
+//         |<---------------(busy)------------------------|
+//         |<---------------(result)----------------------|
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/// All the command codes that can be sent and received between [Detector] and
+/// [_DetectorServer].
+enum _Codes { init, busy, ready, detect, result }
+
+/// A command sent between [Detector] and [_DetectorServer].
+class _Command {
+  const _Command(this.code, {this.args});
+
+  final _Codes code;
+  final List<Object>? args;
+}
+
+/// A Simple Detector that handles object detection via Service
+///
+/// All the heavy operations like pre-processing, detection, ets,
+/// are executed in a background isolate.
+/// This class just sends and receives messages to the isolate.
+class Detector {
+  // static const String _modelPath = 'assets/models/ssd_mobilenet.tflite';
+  // static const String _modelPath = 'assets/models/frets_and_nuts.tflite';
+  static const String _modelPath = 'assets/models/guitar_model.tflite';
+  static const String _labelPath = 'assets/models/labelmap.txt';
+
+  Detector._(this._isolate, this._interpreter, this._labels);
+
+  final Isolate _isolate;
+  late final Interpreter _interpreter;
+  late final List<String> _labels;
+
+  // To be used by detector (from UI) to send message to our Service ReceivePort
+  late final SendPort _sendPort;
+
+  bool _isReady = false;
+
+  // // Similarly, StreamControllers are stored in a queue so they can be handled
+  // // asynchronously and serially.
+  final StreamController<Map<String, dynamic>> resultsStream =
+      StreamController<Map<String, dynamic>>();
+
+  /// Open the database at [path] and launch the server on a background isolate..
+  static Future<Detector> start() async {
+    final ReceivePort receivePort = ReceivePort();
+    // sendPort - To be used by service Isolate to send message to our ReceiverPort
+    final Isolate isolate = await Isolate.spawn(
+      _DetectorServer._run,
+      receivePort.sendPort,
+    );
+
+    final Detector result = Detector._(
+      isolate,
+      await _loadModel(),
+      await _loadLabels(),
+    );
+    receivePort.listen((message) {
+      result._handleCommand(message as _Command);
+    });
+    return result;
+  }
+
+  static Future<Interpreter> _loadModel() async {
+    final interpreterOptions = InterpreterOptions();
+
+    // Use XNNPACK Delegate
+    if (Platform.isAndroid) {
+      interpreterOptions.addDelegate(XNNPackDelegate());
+    }
+
+    return Interpreter.fromAsset(
+      _modelPath,
+      options: interpreterOptions..threads = 4,
+    );
+  }
+
+  static Future<List<String>> _loadLabels() async {
+    return (await rootBundle.loadString(_labelPath)).split('\n');
+  }
+
+  /// Starts CameraImage processing
+  void processFrame(CameraImage cameraImage) {
+    if (_isReady) {
+      _sendPort.send(_Command(_Codes.detect, args: [cameraImage]));
+    }
+  }
+
+  /// Handler invoked when a message is received from the port communicating
+  /// with the database server.
+  void _handleCommand(_Command command) {
+    switch (command.code) {
+      case _Codes.init:
+        _sendPort = command.args?[0] as SendPort;
+        // ----------------------------------------------------------------------
+        // Before using platform channels and plugins from background isolates we
+        // need to register it with its root isolate. This is achieved by
+        // acquiring a [RootIsolateToken] which the background isolate uses to
+        // invoke [BackgroundIsolateBinaryMessenger.ensureInitialized].
+        // ----------------------------------------------------------------------
+        RootIsolateToken rootIsolateToken = RootIsolateToken.instance!;
+        _sendPort.send(
+          _Command(
+            _Codes.init,
+            args: [rootIsolateToken, _interpreter.address, _labels],
+          ),
+        );
+      case _Codes.ready:
+        _isReady = true;
+      case _Codes.busy:
+        _isReady = false;
+      case _Codes.result:
+        _isReady = true;
+        resultsStream.add(command.args?[0] as Map<String, dynamic>);
+      default:
+        debugPrint('Detector unrecognized command: ${command.code}');
+    }
+  }
+
+  /// Kills the background isolate and its detector server.
+  void stop() {
+    _isolate.kill();
+  }
+}
+
+/// The portion of the [Detector] that runs on the background isolate.
+///
+/// This is where we use the new feature Background Isolate Channels, which
+/// allows us to use plugins from background isolates.
+class _DetectorServer {
+  /// Input size of image (height = width = 300)
+  // static const int mlModelInputSize = 300;
+  static const int mlModelInputSize = 640;
+
+  /// Result confidence threshold
+  static const double confidence = 0.5;
+  Interpreter? _interpreter;
+  List<String>? _labels;
+
+  _DetectorServer(this._sendPort);
+
+  final SendPort _sendPort;
+
+  // ----------------------------------------------------------------------
+  // Here the plugin is used from the background isolate.
+  // ----------------------------------------------------------------------
+
+  /// The main entrypoint for the background isolate sent to [Isolate.spawn].
+  static void _run(SendPort sendPort) {
+    ReceivePort receivePort = ReceivePort();
+    final _DetectorServer server = _DetectorServer(sendPort);
+    receivePort.listen((message) async {
+      final _Command command = message as _Command;
+      await server._handleCommand(command);
+    });
+    // receivePort.sendPort - used by UI isolate to send commands to the service receiverPort
+    sendPort.send(_Command(_Codes.init, args: [receivePort.sendPort]));
+  }
+
+  /// Handle the [command] received from the [ReceivePort].
+  Future<void> _handleCommand(_Command command) async {
+    switch (command.code) {
+      case _Codes.init:
+        // ----------------------------------------------------------------------
+        // The [RootIsolateToken] is required for
+        // [BackgroundIsolateBinaryMessenger.ensureInitialized] and must be
+        // obtained on the root isolate and passed into the background isolate via
+        // a [SendPort].
+        // ----------------------------------------------------------------------
+        RootIsolateToken rootIsolateToken =
+            command.args?[0] as RootIsolateToken;
+        // ----------------------------------------------------------------------
+        // [BackgroundIsolateBinaryMessenger.ensureInitialized] for each
+        // background isolate that will use plugins. This sets up the
+        // [BinaryMessenger] that the Platform Channels will communicate with on
+        // the background isolate.
+        // ----------------------------------------------------------------------
+        BackgroundIsolateBinaryMessenger.ensureInitialized(rootIsolateToken);
+        _interpreter = Interpreter.fromAddress(command.args?[1] as int);
+        _labels = command.args?[2] as List<String>;
+        _sendPort.send(const _Command(_Codes.ready));
+      case _Codes.detect:
+        _sendPort.send(const _Command(_Codes.busy));
+        _convertCameraImage(command.args?[0] as CameraImage);
+      default:
+        debugPrint('_DetectorService unrecognized command ${command.code}');
+    }
+  }
+
+  void _convertCameraImage(CameraImage cameraImage) {
+    var preConversionTime = DateTime.now().millisecondsSinceEpoch;
+
+    convertCameraImageToImage(cameraImage).then((image) {
+      if (image != null) {
+        if (Platform.isAndroid) {
+          image = image_lib.copyRotate(image, angle: 90);
+        }
+
+        final results = analyseImage(image, preConversionTime);
+        _sendPort.send(_Command(_Codes.result, args: [results]));
+      }
+    });
+  }
+
+  Map<String, dynamic> analyseImage(
+    image_lib.Image? image,
+    int preConversionTime,
+  ) {
+    var conversionElapsedTime =
+        DateTime.now().millisecondsSinceEpoch - preConversionTime;
+
+    var preProcessStart = DateTime.now().millisecondsSinceEpoch;
+
+    /// Pre-process the image
+    /// Resizing image for model [300, 300]
+    final imageInput = image_lib.copyResize(
+      image!,
+      width: mlModelInputSize,
+      height: mlModelInputSize,
+    );
+
+    // Creating matrix representation, [300, 300, 3]
+    final imageMatrix = List.generate(
+      imageInput.height,
+      (y) => List.generate(imageInput.width, (x) {
+        final pixel = imageInput.getPixel(x, y);
+        return [pixel.r, pixel.g, pixel.b];
+      }),
+    );
+
+    var preProcessElapsedTime =
+        DateTime.now().millisecondsSinceEpoch - preProcessStart;
+
+    var inferenceTimeStart = DateTime.now().millisecondsSinceEpoch;
+
+    final output = _runInference(imageMatrix);
+
+    final formattedOutput = postProcess(output.first, mlModelInputSize.toDouble(), mlModelInputSize.toDouble(), 0.25, 0.8);
+
+    final List<Rect> locations = formattedOutput.map((box) => Rect.fromLTRB(box["rect"][0], box["rect"][1], box["rect"][2], box["rect"][3])).toList();
+    final classes = formattedOutput.map((box) => box["class"]).toList();
+    final scores = formattedOutput.map((box) => box["conf"]).toList();
+    final numberOfDetections = formattedOutput.length;
+
+    // Location
+    // final locationsRaw = output.first.first as List<List<double>>;
+    // final List<Rect> locations = locationsRaw
+    //     .map((list) => list.map((value) => (value * mlModelInputSize)).toList())
+    //     .map((rect) => Rect.fromLTRB(rect[1], rect[0], rect[3], rect[2]))
+    //     .toList();
+
+
+
+    // Classes
+    // final classesRaw = output.elementAt(1).first as List<double>;
+    // final classes = classesRaw.map((value) => value.toInt()).toList();
+
+
+
+    // Scores
+    // final scores = output.elementAt(2).first as List<double>;
+
+
+    // Number of detections
+    // final numberOfDetectionsRaw = output.last.first as double;
+    // final numberOfDetections = numberOfDetectionsRaw.toInt();
+
+    final List<String> classification = [];
+    for (var i = 0; i < numberOfDetections; i++) {
+      classification.add(_labels![classes[i]]);
+    }
+
+    /// Generate recognitions
+    List<Recognition> recognitions = [];
+    for (int i = 0; i < numberOfDetections; i++) {
+      // Prediction score
+      var score = scores[i];
+      // Label string
+      var label = classification[i];
+
+      if (score > confidence) {
+        recognitions.add(Recognition(i, label, score, locations[i]));
+      }
+    }
+
+    var inferenceElapsedTime =
+        DateTime.now().millisecondsSinceEpoch - inferenceTimeStart;
+
+    var totalElapsedTime =
+        DateTime.now().millisecondsSinceEpoch - preConversionTime;
+
+    return {
+      "recognitions": recognitions,
+      "stats": <String, String>{
+        'Conversion time:': conversionElapsedTime.toString(),
+        'Pre-processing time:': preProcessElapsedTime.toString(),
+        'Inference time:': inferenceElapsedTime.toString(),
+        'Total prediction time:': totalElapsedTime.toString(),
+        'Frame': '${image.width} X ${image.height}',
+      },
+    };
+  }
+
+  /// Object detection main function
+  List<List<Object>> _runInference(List<List<List<num>>> imageMatrix) {
+    // Set input tensor [1, 300, 300, 3]
+    final input = [imageMatrix];
+
+    // Set output tensor
+    // Locations: [1, 10, 4]
+    // Classes: [1, 10],
+    // Scores: [1, 10],
+    // Number of detections: [1]
+    final output = {
+      0: [List<List<num>>.filled(7, List<num>.filled(8400, 0))],
+      1: [List<double>.filled(10, 0)],
+      2: [List<double>.filled(10, 0)],
+      3: [0.0],
+    };
+
+    _interpreter!.runForMultipleInputs([input], output);
+    return output.values.toList();
+  }
+
+  // This function applies the post-processing
+  static List<Map<String, dynamic>> postProcess(
+    List<dynamic> rawOutput,
+    double imageWidth,
+    double imageHeight,
+    double confThreshold,
+    double nmsThreshold,
+  ) {
+    List<Map<String, dynamic>> boxes = [];
+
+    // Step 1: Process raw outputs
+    for (var detection in rawOutput) {
+      double x = detection[0]; // x-center
+      double y = detection[1]; // y-center
+      double w = detection[2]; // width
+      double h = detection[3]; // height
+      double confidence = detection[4]; // confidence score
+      int classId = detection[5].toInt(); // class id
+      double classConfidence = detection[6]; // confidence of the class
+
+      // Step 2: Apply confidence threshold
+      if (confidence * classConfidence < confThreshold) {
+        continue; // Skip this detection if confidence is too low
+      }
+
+      // Step 3: Convert bounding box to pixel values
+      double left = (x - w / 2) * imageWidth;
+      double top = (y - h / 2) * imageHeight;
+      double right = (x + w / 2) * imageWidth;
+      double bottom = (y + h / 2) * imageHeight;
+
+      // Store the box as a map for easier access later
+      boxes.add({
+        'class': classId,
+        'confidence': confidence * classConfidence,
+        'rect': [left, top, right, bottom], // bounding box coordinates
+      });
+    }
+
+    // Step 4: Apply Non-Maximum Suppression (NMS)
+    return applyNMS(boxes, nmsThreshold);
+  }
+
+  // Apply Non-Maximum Suppression (NMS) to remove redundant boxes
+  static List<Map<String, dynamic>> applyNMS(
+    List<Map<String, dynamic>> boxes,
+    double threshold,
+  ) {
+    if (boxes.isEmpty) return boxes;
+
+    // Sort boxes by confidence score in descending order
+    boxes.sort((a, b) => b['confidence'].compareTo(a['confidence']));
+
+    List<Map<String, dynamic>> nmsBoxes = [];
+    List<bool> isBoxRemoved = List.generate(boxes.length, (_) => false);
+
+    for (int i = 0; i < boxes.length; i++) {
+      if (isBoxRemoved[i]) continue; // Skip if the box is already removed
+      Map<String, dynamic> currentBox = boxes[i];
+      nmsBoxes.add(currentBox);
+
+      for (int j = i + 1; j < boxes.length; j++) {
+        if (isBoxRemoved[j]) continue;
+
+        Map<String, dynamic> compareBox = boxes[j];
+
+        // Calculate the intersection over union (IoU)
+        double iou = calculateIoU(currentBox['rect'], compareBox['rect']);
+
+        if (iou > threshold) {
+          // Mark the box as removed if the IoU is above the threshold
+          isBoxRemoved[j] = true;
+        }
+      }
+    }
+
+    return nmsBoxes;
+  }
+
+  // Calculate the Intersection over Union (IoU) between two bounding boxes
+  static double calculateIoU(List<double> box1, List<double> box2) {
+    double x1 = max(box1[0], box2[0]);
+    double y1 = max(box1[1], box2[1]);
+    double x2 = min(box1[2], box2[2]);
+    double y2 = min(box1[3], box2[3]);
+
+    double intersectionArea = max(0, x2 - x1) * max(0, y2 - y1);
+    double box1Area = (box1[2] - box1[0]) * (box1[3] - box1[1]);
+    double box2Area = (box2[2] - box2[0]) * (box2[3] - box2[1]);
+
+    double unionArea = box1Area + box2Area - intersectionArea;
+
+    return intersectionArea / unionArea;
+  }
+}
